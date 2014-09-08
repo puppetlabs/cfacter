34cf99cf7b106b0d14aca419c2421cea628bb4e5
Rahul Gopinath <rahul@puppetlabs.com>
2014-09-08 11:20:41 -0700

    (CFACT 92) convert kstat to scoped resource
    Before this patch, the kstat_ctl pointer was managed by hand. This patch introduces
    the scoped kstat resource which manages the pointer's scope automatically.

--- lib/src/facts/solaris/memory_resolver.cc
+++ lib/src/facts/solaris/memory_resolver.cc
@@ -1,13 +1,14 @@
 #include <facter/facts/solaris/memory_resolver.hpp>
 #include <facter/logging/logging.hpp>
 #include <facter/util/file.hpp>
+#include <facter/util/solaris/scoped_kstat.hpp>
 #include <boost/algorithm/string.hpp>
-#include <kstat.h>
 #include <sys/sysinfo.h>
 #include <sys/sysconfig.h>
 
 using namespace std;
 using namespace facter::util;
+using namespace facter::util::solaris;
 LOG_DECLARE_NAMESPACE("facts.solaris.memory");
 
 namespace facter { namespace facts { namespace solaris {
@@ -19,50 +20,44 @@ namespace facter { namespace facts { namespace solaris {
             uint64_t& swap_total)
     {
         long page_size = sysconf(_SC_PAGESIZE);
-        kstat_ctl_t *kc;
-        if ((kc = kstat_open()) == nullptr) {
-            LOG_DEBUG("kstat_open failed: %1% (%2%).", strerror(errno), errno);
+        try {
+            scoped_kstat kc;
+            if (kc == nullptr) {
+                throw kstat_exception("kstat_open failed");
+            }
+
+            kstat_t *kp;
+            kstat_named_t *knp;
+
+            if ((kp = kstat_lookup(kc, const_cast<char*>("unix"), 0, const_cast<char *>("system_pages"))) == nullptr) {
+                throw kstat_exception("kstat_lookup failed");
+            }
+
+            if (kstat_read(kc, kp, 0) == -1) {
+                throw kstat_exception("kstat_read failed");
+            }
+
+            if ((knp = reinterpret_cast<kstat_named_t*>(kstat_data_lookup(kp, const_cast<char *>("physmem")))) == nullptr) {
+                throw kstat_exception("kstat_data_lookup failed");
+            }
+            mem_total = knp->value.ul * page_size;
+
+            if ((knp = reinterpret_cast<kstat_named_t*>(kstat_data_lookup(kp, const_cast<char *>("pagesfree")))) == nullptr) {
+                throw kstat_exception("kstat_lookup failed");
+            }
+            mem_free = knp->value.ul * page_size;
+
+            // Swap requires a little more investigation as to the correct method. See
+            // https://community.oracle.com/thread/1951228?start=0&tstart=0
+            // http://www.brendangregg.com/K9Toolkit/swapinfo
+            swap_free = 0;
+            swap_total = 0;
+
+            return true;
+        } catch (exception &ex) {
+            LOG_DEBUG("%1%: memory facts unavailable (%2%)", ex.what(), strerror(errno));
             return facter::facts::posix::memory_resolver::get_memory_statistics(facts, mem_free, mem_total, swap_free, swap_total);
         }
-
-        kstat_t *kp;
-        kstat_named_t *knp;
-
-        if ((kp = kstat_lookup(kc, const_cast<char*>("unix"), 0, const_cast<char *>("system_pages"))) == nullptr) {
-            kstat_close(kc);
-            LOG_DEBUG("kstat_lookup failed: memory facts unavailable %1% (%2%)", strerror(errno), errno);
-            return facter::facts::posix::memory_resolver::get_memory_statistics(facts, mem_free, mem_total, swap_free, swap_total);
-        }
-
-        if (kstat_read(kc, kp, 0) == -1) {
-            kstat_close(kc);
-            LOG_DEBUG("kstat_lookup failed: memory facts unavailable %1% (%2%)", strerror(errno), errno);
-            return facter::facts::posix::memory_resolver::get_memory_statistics(facts, mem_free, mem_total, swap_free, swap_total);
-        }
-
-        if ((knp = reinterpret_cast<kstat_named_t*>(kstat_data_lookup(kp, const_cast<char *>("physmem")))) == nullptr) {
-            kstat_close(kc);
-            LOG_DEBUG("kstat_lookup failed: memory facts unavailable %1% (%2%)", strerror(errno), errno);
-            return facter::facts::posix::memory_resolver::get_memory_statistics(facts, mem_free, mem_total, swap_free, swap_total);
-        }
-        mem_total = knp->value.ul * page_size;
-
-        if ((knp = reinterpret_cast<kstat_named_t*>(kstat_data_lookup(kp, const_cast<char *>("pagesfree")))) == nullptr) {
-            kstat_close(kc);
-            LOG_DEBUG("kstat_lookup failed: memory facts unavailable %1% (%2%)", strerror(errno), errno);
-            return facter::facts::posix::memory_resolver::get_memory_statistics(facts, mem_free, mem_total, swap_free, swap_total);
-        }
-        mem_free = knp->value.ul * page_size;
-
-        kstat_close(kc);
-
-        // Swap requires a little more investigation as to the correct method. See
-        // https://community.oracle.com/thread/1951228?start=0&tstart=0
-        // http://www.brendangregg.com/K9Toolkit/swapinfo
-        swap_free = 0;
-        swap_total = 0;
-
-        return true;
     }
 
 }}}  // namespace facter::facts::solaris
