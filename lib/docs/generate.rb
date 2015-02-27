adecafdd12e66a0ae01213411606c7e41504fdc3
Peter Huene <peter.huene@puppetlabs.com>
2015-02-27 13:25:16 -0800

    (CFACT-158) Add script to generate fact documentation markdown.
    Adding lib/docs/generate.rb (usage: ruby generate.rb > facts.md).
    This script will transform the schema file into a markdown and extract
    the name, purpose, resolutions, and caveats for each fact.

---
+++ lib/docs/generate.rb
@@ -0,0 +1,12 @@
+require 'yaml'
+require 'erb'
+require 'ostruct'
+
+PATH_TO_SCHEMA = File.join(File.dirname(__FILE__), '../schema/facter.yaml')
+PATH_TO_TEMPLATE = File.join(File.dirname(__FILE__), 'template.erb')
+
+scope = OpenStruct.new({
+  :facts => YAML.load_file(PATH_TO_SCHEMA)
+})
+
+puts ERB.new(File.read(PATH_TO_TEMPLATE), nil, '-').result(scope.instance_eval {binding})
